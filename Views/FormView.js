1a9ecd30c7bd44001f46c00a8a5bbc4f2d7c047b
Dennis Eichhorn <spl1nes.com@googlemail.com>
2016-03-19 12:22:42 +0100

    Form view draft

--- Views/FormView.js
+++ Views/FormView.js
@@ -1,14 +1,37 @@
 (function (jsOMS, undefined) {
-    jsOMS.FormView = function (node) {
-        if (node) {
-            this.setNode(node);
-        }
+    "use strict";
+    
+    jsOMS.FormView = function (element) {
+        this.id = element.getAttribute('id');
+        this.formElement = element;
+
+        this.inputElement = {};
+        this.textarea = {};
+        this.button = {};
+        this.select = {};
     };
 
-    jsOMS.FormView.prototype.setNode = function () {
-    };
+    jsOMS.FormView.prototype.bind = function() 
+    {
+        this.bindInput();
+        this.bindTextarea();
+        this.bindButton();
+        this.bindSelect();
+    }
 
-    jsOMS.FormView.prototype.submit = function () {
+    jsOMS.FormView.prototype.bindInput = function() 
+    {
+        var self = this;
 
-    };
+        let inputs = this.formElement.getElementsByTagName('input');
+
+        Object.keys(inputs).forEach(function (key, element) {
+            self.inputElement[element.getAttribute('id')] = {
+                id: element.getAttribute('id'),
+                type: element.getAttribute('type')
+            };
+        });
+    }
+
+    jsOMS.FormView.prototype.bind
 }(window.jsOMS = window.jsOMS || {}));
