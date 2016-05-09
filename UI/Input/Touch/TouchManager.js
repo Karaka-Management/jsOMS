d11e9cf415196011b5042b147012ec8a7f4d4dda
Dennis Eichhorn <spl1nes.com@googlemail.com>
2016-05-09 13:59:07 +0200

    Update TouchManager.js

--- UI/Input/Touch/TouchManager.js
+++ UI/Input/Touch/TouchManager.js
@@ -1,1 +1,31 @@
-// todo: implement
+
+/**
+ * Touch manager class.
+ *
+ * @author     OMS Development Team <dev@oms.com>
+ * @author     Dennis Eichhorn <d.eichhorn@oms.com>
+ * @copyright  2013 Dennis Eichhorn
+ * @license    OMS License 1.0
+ * @version    1.0.0 * @since      1.0.0
+ */
+(function (jsOMS, undefined)
+{
+    jsOMS.Autoloader.defineNamespace('jsOMS.UI.Input.Touch');
+
+    /**
+     * @constructor
+     *
+     * @since 1.0.0
+     * @author Dennis Eichhorn <d.eichhorn@oms.com>
+     */
+    jsOMS.UI.Input.Touch.TouchManager = function (app)
+    {
+        this.app    = app;
+        this.surfaces = {};
+    };
+
+    jsOMS.UI.FormManager.prototype.add = function (id, callback)
+    {
+    };
+    
+}(window.jsOMS = window.jsOMS || {}));
