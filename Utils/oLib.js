4dff9197c64f62188b8059b6b86fc2c4b102a3e4
Dennis Eichhorn <spl1nes.com@googlemail.com>
2017-05-31 10:54:19 +0200

    Added substr_count

--- Utils/oLib.js
+++ Utils/oLib.js
@@ -23,6 +23,32 @@
     {
         return str.replace(new RegExp("^[" + char + "]*"), '');
     };
+    
+    jsOMS.substr_count = function(str, substr) {
+        str += '';
+        substr += '';
+        
+        if (substr.length <= 0) { 
+            return (str.length + 1);
+        }
+
+        let n = 0,
+            pos = 0,
+            step = substr.length;
+
+        while (true) {
+            pos = str.indexOf(substr, pos);
+            
+            if (pos >= 0) {
+                ++n;
+                pos += step;
+            } else {
+                break;
+            }
+        }
+        
+        return n;
+    }
 
     /**
      * Class finder
