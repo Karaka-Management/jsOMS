92795ae265c86f4115ad4841d41012ac40a5b439
Dennis Eichhorn <spl1nes.com@googlemail.com>
2017-06-03 21:34:51 +0200

    Fix file documentation

--- UnhandledException.js
+++ UnhandledException.js
@@ -5,7 +5,8 @@
  * @author     Dennis Eichhorn <d.eichhorn@oms.com>
  * @copyright  2013 Dennis Eichhorn
  * @license    OMS License 1.0
- * @version    1.0.0 * @since      1.0.0
+ * @version    1.0.0
+ * @since      1.0.0
  */
 (function ()
 {
