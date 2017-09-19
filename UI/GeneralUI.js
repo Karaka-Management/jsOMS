de25c32501c4fa3127d467dd23d0831049d18200
Dennis Eichhorn <spl1nes.com@googlemail.com>
2017-09-19 14:56:39 +0200

    fixes #34

--- UI/GeneralUI.js
+++ UI/GeneralUI.js
@@ -20,6 +20,8 @@
      */
     jsOMS.UI.GeneralUI = function ()
     {
+
+        this.visObs = null;
     };
     
     /**
@@ -39,6 +41,7 @@
         } 
         
         this.bindHref(e);
+        this.bindLazyLoad(e);
     };
     
     /**
@@ -60,6 +63,42 @@
                 jsOMS.preventAll(event);
                 window.location = jsOMS.Uri.UriFactory.build(this.getAttribute('data-href'));
             });
+        }
+    };
+
+    /**
+     * Bind & rebind UI element.
+     *
+     * @param {Object} [e] Element id
+     *
+     * @method
+     *
+     * @since  1.0.0
+     */
+    jsOMS.UI.GeneralUI.prototype.bindLazyLoad = function (e)
+    {
+        e = e !== null ? e.querySelectorAll('[data-lazyload]') : document.querySelectorAll('[data-lazyload]');
+        const length = e.length;
+
+        if(!this.visObs && window.IntersectionObserver) {
+            this.visObs = new IntersectionObserver(function(eles, obs) {
+                eles.forEach(ele => {
+                    if (ele.intersectionRatio > 0) {
+                        obs.unobserve(ele.target);
+                        ele.target.src = ele.target.dataset.lazyload;
+                        delete ele.target.dataset.lazyload;
+                    }
+                });
+            });
+        }
+
+        for(let i = 0; i < length; i++) {
+            if(!this.visObs) {
+                e[i].src = e[i].dataset.lazyload;
+                delete e[i].dataset.lazyload;
+            } else {
+                this.visObs.observe(e[i]);
+            }
         }
     };
 }(window.jsOMS = window.jsOMS || {}));
