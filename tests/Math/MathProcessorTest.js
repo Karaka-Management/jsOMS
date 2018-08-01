75c860b6b7fb23413034edb7d5e2469fb8f863e0
Dennis Eichhorn <spl1nes.com@googlemail.com>
2018-08-01 22:09:27 +0200

    Add math bug test

--- tests/Math/MathProcessorTest.js
+++ tests/Math/MathProcessorTest.js
@@ -2,13 +2,14 @@ describe('MathProcessorTest', function ()
 {
     "use strict";
 
-    describe('testEvaluation', function ()
+    describe('testBasicEvaluation', function ()
     {
         it('Testing formula evaluation', function ()
         {
             expect(jsOMS.mathEvaluate('3 + 4 * 2 / ( 1 - 5 ) ^ 2 ^ 3 + 1.5')).toBeCloseTo(4.5, 2);
             expect(jsOMS.mathEvaluate('3+4*2/(1-5)^2^3+1.5')).toBeCloseTo(4.5, 2);
             expect(jsOMS.mathEvaluate('invalid')).toBe(null);
+            expect(jsOMS.mathEvaluate('3+4*2/(1-5^2^3+1.5')).toBe(null);
         });
     });
 });
